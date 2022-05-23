6414ff0c2f500a082c04e1799500016b53ca8b88
VaXinNavi <ivan_bendotti@yahoo.com>
2022-05-23 14:45:56 +0200

    Inserito metodo listaDiTutteLeDescrizioniDegliUtentiAssociati

--- src/main/java/it/manytomanyjpamaven/service/RuoloService.java
+++ src/main/java/it/manytomanyjpamaven/service/RuoloService.java
@@ -17,6 +17,8 @@ public interface RuoloService {
 	public void rimuovi(Long idRuoloToRemove) throws Exception;
 
 	public Ruolo cercaPerDescrizioneECodice(String descrizione, String codice) throws Exception;
+	
+	public List<Ruolo> listaDiTutteLeDescrizioniDegliUtentiAssociati() throws Exception;
 
 	// per injection
 	public void setRuoloDAO(RuoloDAO ruoloDAO);
