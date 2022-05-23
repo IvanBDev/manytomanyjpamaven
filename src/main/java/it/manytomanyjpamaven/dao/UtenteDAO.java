6d3fb6dd171a6479eafcc531085e7aa7e4fd1f42
VaXinNavi <ivan_bendotti@yahoo.com>
2022-05-23 14:47:53 +0200

    Inserito metodo findAllByDateOfJune2021

--- src/main/java/it/manytomanyjpamaven/dao/UtenteDAO.java
+++ src/main/java/it/manytomanyjpamaven/dao/UtenteDAO.java
@@ -1,5 +1,6 @@
 package it.manytomanyjpamaven.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import it.manytomanyjpamaven.model.Ruolo;
@@ -8,6 +9,9 @@ import it.manytomanyjpamaven.model.Utente;
 public interface UtenteDAO extends IBaseDAO<Utente> {
 	
 	public List<Utente> findAllByRuolo(Ruolo ruoloInput);
+	
 	public Utente findByIdFetchingRuoli(Long id);
+	
+	public List<Utente> findAllByDateOfJune2021(Date dataInput) throws Exception;
 
 }
